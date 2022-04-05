dc00f176c1ab299fa9b34786ad977e8bb9646c90
Natanael Varela <natanael.varela98@gmail.com>
2022-04-05 12:39:59 -0300

    papa.html comitiado

--- Mi_Primer_MVT/Mi_Primer_MVT/urls.py
+++ Mi_Primer_MVT/Mi_Primer_MVT/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from .views import calcular_fecha, probandoTemplate, saluda, saluda2, saluda_con_nombre, segundoTemplate
+from MvtApp.views import papa, inicio, mama, hermano
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludar/', saluda),
-    path('saludar2/', saluda2),
-    path('saluda-nombre/<nombre>/<edad>', saluda_con_nombre),
-    path('calcular-fecha/<nombre>/<edad>', calcular_fecha),
-    path('probandoTemplate/<nombre>', probandoTemplate),
-    path('segundoTemplate/<nombre>', segundoTemplate),
+    path('inicio/', inicio),
+    path('papa/<nombre>/<edad>/<fecha_nac>/', papa),
+    path('mama/<nombre>/<edad>/<fecha_nac>/<idioma>', mama),
+    path('hermano/<nombre>/<edad>/<fecha_nac>/', hermano),
 ]
--- Mi_Primer_MVT/Mi_Primer_MVT/views.py
+++ Mi_Primer_MVT/Mi_Primer_MVT/views.py
@@ -2,42 +2,11 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.template import Context, Template
 from django.template import loader
-
-def saluda(request):
-
-    return HttpResponse('Hola Mundo!')
-
-
-def saluda2(request):
-
-    return HttpResponse('Hola desde mi segundo template')
-
-
-def saluda_con_nombre(request, nombre, edad):
-
-    return HttpResponse(f'Hola {nombre} tenes {edad} años!')
-
-
-def calcular_fecha(request, nombre, edad):
-
-    año_nacimiento = 2022 - int(edad)
-
-    return HttpResponse(f'Hola {nombre} naciste en {año_nacimiento}!')
-
+    
 
 def probandoTemplate(request, nombre):
 
     listaNotas = [2, 3, 4, 8, 10]
-
-#    miHtml = open('C:/Users/ezequ/Desktop/Natu/1. Programación/1. Curso CoderHouse/mi_primer_mvt/Mi_Primer_MVT/Mi_Primer_MVT/templates/template1.html')
-
-#    plantilla = Template(miHtml.read())
-
-#    miHtml.close()
-
-#    miContexto = Context({"name": nombre, "ahora": datetime.now(), "notas": listaNotas})
-
-#    return HttpResponse(plantilla.render(miContexto))
 
     plantilla = loader.get_template('template1.html')
 
